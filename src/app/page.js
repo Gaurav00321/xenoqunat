788f4a2c0e5eba306c137ec6e8d833c225267b4c
Gaurav Upadhyay <gauravupadhyay20112003@gmail.com>
2025-02-13 23:23:19 +0530

    hogya

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import Navbar from "@/components/navbar";
-import Hero from "@/components/Hero"
+import Hero from "@/components/Hero";
+import { Brands } from "@/components/Brands";
+import { FocusCardsDemo } from "@/components/Influencer";
+import { StickyScrollRevealDemo } from "@/components/team";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero/>
+      <StickyScrollRevealDemo />
+      <Brands />
+      <FocusCardsDemo />
     </main>
   );
 }

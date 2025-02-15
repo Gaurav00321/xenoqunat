f62573a5fb3ca50f0d97c58f3ab7db842f4470f4
Gaurav Upadhyay <gauravupadhyay20112003@gmail.com>
2025-02-16 02:34:58 +0530

    temp

--- src/components/case-study.js
+++ src/components/case-study.js
@@ -9,34 +9,27 @@ export const products = [
   {
     title: "Prince Gaming",
     link: "https://youtube.com/@princegaming575753?si=NkxEJZFni4cfUIQA",
-    thumbnail:
-      "/Prince_Gaming.png",
+    thumbnail: "/Prince_Gaming.png",
   },
   {
     title: "hk2fy",
     link: "https://www.youtube.com/@brhk2fy",
-    thumbnail:
-      "/hk2fy.png",
+    thumbnail: "/hk2fy.png",
   },
   {
     title: "AM_Flash_Gamer",
     link: "https://www.youtube.com/@amflashgamer",
-    thumbnail:
-      "/AM Flash Gamer.png",
+    thumbnail: "/AM_Flash_Gamer.png",
   },
 
   {
     title: "Razzee Gaming",
     link: "https://www.youtube.com/@razzeegaming",
-    thumbnail:
-      "/Razzee_Gaming.png",
+    thumbnail: "/Razzee_Gaming.png",
   },
   {
     title: "Preet X",
     link: "https://www.youtube.com/@preeet_x",
-    thumbnail:
-      "/Preet_X.png",
+    thumbnail: "/Preet_X.png",
   },
-
-  
 ];
